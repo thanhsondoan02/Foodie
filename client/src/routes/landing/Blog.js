93c8f998e4fd1d830d63f1eb97dc4fbff60974d6
tqh272 <tqhung272@gmail.com>
2023-12-27 09:42:25 +0700

    blog fixed 2

--- client/src/routes/landing/Blog.js
+++ client/src/routes/landing/Blog.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';  // Import motion from framer-motion
 import ResetLocation from '../../helpers/ResetLocation';
 import axios from 'axios';
 
 function Blog() {
-  const baseUrl = 'https://2e577fbb-d06d-412f-b4ea-3ef3d13a77e5.mock.pstmn.io';
+  const baseUrl = 'https://bc36cc0f-d221-4d0b-bc41-dbebf2e5716f.mock.pstmn.io/getBlog';
 
   const [blogData, setBlogData] = useState({
     title: '',
@@ -15,26 +15,36 @@ function Blog() {
 
   useEffect(() => {
     axios
-      .get(`${baseUrl}/blog`)
+      .get(baseUrl)
       .then((res) => {
         setBlogData(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  // Ensure that blogData.posts is an array before mapping over it
+  const posts = blogData.posts || [];
+
   return (
-    <motion.div
+    <motion.div  // Use motion.div for animations
       className="section-9 flex-container flex-column"
       initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 2 }}
     >
       <h2 className="txt-center pop-font txt-white">{blogData.title}</h2>
       <p className="section-description">{blogData.description}</p>
       <section className="blog-posts-landing">
-        {blogData.posts.map((post) => (
-          <section key={post.id} className="post flex-container flex-column">
+        {posts.map((post) => (
+          <motion.section  // Use motion.section for individual post animations
+            key={post.id}
+            className="post flex-container flex-column"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
             <img src={post.img} alt={post.name} />
             <p className="date">{post.date}</p>
             <Link
@@ -44,7 +54,7 @@ function Blog() {
               <h3 className="pop-font txt-white">{post.name}</h3>
             </Link>
             <p className="intro">{post.intro}</p>
-          </section>
+          </motion.section>
         ))}
       </section>
 
